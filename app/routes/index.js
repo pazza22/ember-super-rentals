b0102cefef28e6b2daf06fcb2539bd0ef8996467
Praneeth Praneeth <Praneeth.Praneeth@allscripts.com>
2021-03-11 20:47:27 +0530

    Added Rental details - more routing and templates

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,18 +6,20 @@ export default class IndexRoute extends Route {
   async model() {
     let response = await fetch('/api/rentals.json');
     let { data } = await response.json();
-    
+    console.log("data", data);
     return data.map((model) => {
-        let { attributes } = model;
+        let { id, attributes } = model; //id and attributes of that model
         let type;
+        console.log("\nmodel", model);
+        console.log("id", id);
+        console.log("attributes", attributes);
   
         if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
           type = 'Community';
         } else {
           type = 'Standalone';
         }
-  
-        return { type, ...attributes };
+        return { id, type, ...attributes }; //returning new model
       });
   }
 }
