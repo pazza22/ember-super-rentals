bb89114a91e2f924f17a4407b74e836eea9966e5
Praneeth Praneeth <Praneeth.Praneeth@allscripts.com>
2021-03-12 13:42:21 +0530

    ember data stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,11 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store;
+
   async model() {
-    let response = await fetch('/api/rentals.json');
-    let { data } = await response.json();
-    console.log("data", data);
-    return data.map((model) => {
-        let { id, attributes } = model; //id and attributes of that model
-        let type;
-        console.log("\nmodel", model);
-        console.log("id", id);
-        console.log("attributes", attributes);
-  
-        if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-          type = 'Community';
-        } else {
-          type = 'Standalone';
-        }
-        return { id, type, ...attributes }; //returning new model
-      });
+    console.log("index route", this.store.findAll('rental'));
+    return this.store.findAll('rental');
   }
 }
